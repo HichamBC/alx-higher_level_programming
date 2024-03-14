c5f19cb2e544ddad5e72ddff3ec7f968bd33c06d
HichamBC <yours.hichambc@gmail.com>
2024-03-14 11:33:52 +0000

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def main():
+    add = __import__('add_0').add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
+if __name__ == "__main__":
+    main()
