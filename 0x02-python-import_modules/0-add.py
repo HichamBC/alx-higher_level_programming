6b70ebd9093c24780654c0911ac186839749d23f
HichamBC <yours.hichambc@gmail.com>
2024-03-14 11:39:30 +0000

    0-add.py modified

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def main():
+if __name__ == "__main__":
     add = __import__('add_0').add
     a = 1
     b = 2
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
-
-
-if __name__ == "__main__":
-    main()
