508d86335e51ae98ba54c8dfe7040db4e1c22600
HichamBC <yours.hichambc@gmail.com>
2024-03-17 18:33:11 +0000

    5-no_c.py modified

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -10,24 +10,13 @@ def no_c(my_string):
         str: The modified string with 'c' and 'C' removed.
     """
 
-    # checks if my_string is empty
-    if not my_string:
-        # returns None of True
-        return None
-
-    # Initialize an empty string to store the result
     s = ""
-    # checks if "c" or "C" exists in my_string.
-    if "c" in my_string or "C" in my_string:
-        # Iterate through each character in the string
-        for letter in my_string:
-            # Check if the character is not 'c' or 'C'
-            if letter != "c" and letter != "C":
-                # Append the character to s
-                s += letter
+    # Iterate through each character in the string
+    for letter in my_string:
+        # Check if the character is not 'c' or 'C'
+        if letter != "c" and letter != "C":
+            # Append the character to s
+            s += letter
 
-        # return the modified string s
-        return s
-
-    # return the original string if any of "c" or "C" don't exist in it.
-    return my_string
+    # return the modified string s
+    return s
