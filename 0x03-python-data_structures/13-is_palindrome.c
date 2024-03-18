3260219dbd00c77a265e0d1a873f9eaaaaf38a7f
HichamBC <yours.hichambc@gmail.com>
2024-03-18 18:29:58 +0000

    added linked_list_to_array function

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,41 @@
 #include "lists.h"
 
 /**
+ * linked_list_to_array - Converts a linked list to an array
+ * @head: Pointer to the head of the linked list
+ * @array: Pointer to store the resulting array
+ *
+ * Return: The number of elements in the resulting array
+ */
+
+int linked_list_to_array(listint_t *head, int **array)
+{
+	listint_t *p = head;
+	int count = 0;
+	int i;
+
+	while (p != NULL)
+	{
+		count++;
+		p = p->next;
+	}
+
+	*array = malloc(sizeof(int) * count);
+	if (*array == NULL)
+		return (0);
+
+	p = head;
+	for (i = 0; i < count; i++)
+	{
+		(*array)[i] = p->n;
+		p = p->next;
+	}
+
+	return (count);
+}
+
+
+/**
  * is_palindrome - Checks if a singly linked list is a palindrome
  *
  * @head: Pointer to a pointer to the head node of the linked list
@@ -15,35 +50,19 @@ int is_palindrome(listint_t **head)
 	int i;
 	int j;
 	int *arr = NULL;
-	int count = 0;
 	int is_palindrome = 1;
-	listint_t *ptr = *head;
 
-	while (ptr != NULL)
-	{
-		count++;
-		ptr = ptr->next;
-	}
+	if (head == NULL || *head == NULL)
+		return (1);
 
-	arr = malloc(sizeof(int) * count);
-	if (arr == NULL)
-	{
-		return (0);
-	}
 
-	ptr = *head;
-	for (i = 0; i < count; i++)
-	{
-		arr[i] = ptr->n;
-		ptr = ptr->next;
-	}
+	j = linked_list_to_array(*head, &arr) - 1;
 
-	for (i = 0, j = count - 1; i < j; i++, j--)
+	for (i = 0; i < j; i++, j--)
 	{
 		if (arr[i] != arr[j])
 		{
 			is_palindrome = 0;
-			free(arr);
 			break;
 		}
 	}
