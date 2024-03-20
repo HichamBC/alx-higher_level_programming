01e11dc2add45f58578534e5942a820bb9293483
HichamBC <yours.hichambc@gmail.com>
2024-03-20 17:48:01 +0000

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+
+    rom_nums = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            }
+    res = 0
+    for num in range(len(roman_string) - 1, -1, -1):
+        current = rom_nums[roman_string[num]]
+        if num != len(roman_string) - 1:
+            prev = rom_nums[roman_string[num + 1]]
+        if num != len(roman_string) - 1 and current < prev:
+            res -= current
+        else:
+            res += current
+
+    return res
