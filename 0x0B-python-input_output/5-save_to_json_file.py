79ab9849d5fca6443e86f934b9c4c2609116e2d2
HichamBC <yours.hichambc@gmail.com>
2024-04-16 18:29:34 +0100

    5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -18,5 +18,6 @@ def save_to_json_file(my_obj, filename):
     Returns:
         None
     """
-    with open(filename, "w", encoding="UTF8") as f:
-        json.dumps(my_obj, f)
+    json_str = json.dumps(my_obj)
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(json_str)
